12f601980b5d26ced823f51256a9da70bcbbbc77
skyxingcheng233 <skyxingcheng233@outlook.com>
2026-07-16 10:04:29 +0800

    feat: 分离配置

---
+++ conf.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+export FUNNY_LIST=(
+  "alibaba"
+  "netease"
+  "tencent"
+  "douyin"
+  "bilibili"
+  "doubao"
+  "iqiyi"
+)
+
+export DOMAIN_URLS=(
+  "oracle"
+  "azure"
+  "aws"
+  "dmm"
+)
+
+export ASNLIST=(
+  "AS13335"   # Cloudflare
+  "AS54113"   # Fastly
+  "AS60068"   # Bunny
+  "AS199524"  # Gcore
+  "AS20940"   # Akamai
+  "AS16625"   # Akamai
+  "AS32787"   # Akamai
+  # END CDN
+  "AS3462"    # Hinet
+  "AS4641"    # HKIX
+  "AS9269"    # HKBN
+  "AS4760"    # HKT
+  "AS3491"    # PCCW
+  "AS9908"    # iCable
+  "AS31898"   # OCI
+  "AS400618"  # RFCHost
+  "AS17433"   # Hytron
+  "AS202662"  # Hytron
+  "AS151407"  # Hytron
+  "AS401434"  # Hytron
+  "AS12027"   # Hytron
+  "AS16276"   # OVH
+  "AS197540"  # Netcup
+  "AS151487"  # Awesomecloud
+  "AS48266"   # Catixs
+  "AS53808"   # MoeDove
+  "AS55933"   # Cloudie
+  "AS976"     # CoreNET
+  "AS132839"  # POWER LINE
+  "AS62468"   # VpsQuan
+  "AS8075"    # Azure
+  "AS396982"  # GCP
+  "AS16509"   # AWS
+  "AS398810"  # MXroute
+  "AS13238"   # Yandex
+  "AS42960"   # VH Global Limited
+  "AS36352"   # HostPapa(CCS)
+  "AS36149"   # Hawaiian Telcom
+  "AS61112"   # AKILE
+  "AS402279"  # Wawo
+  "AS131642"  # PQS
+)
