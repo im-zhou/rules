6d6ab02858ef3f402a064d3ca81053eb21bf6937
skyxingcheng233 <skyxingcheng233@outlook.com>
2026-08-20 11:29:56 +0800

    add

--- conf.sh
+++ conf.sh
@@ -82,4 +82,5 @@ export ASNLIST=(
   "AS137872"  # China Mobile Hong Kong Company Limited
   "AS9231"    # China Mobile Hong Kong Company Limited
   "AS20473"   # Vultr
+  "AS50118"   # Caribbean Telecom S.A
 )
